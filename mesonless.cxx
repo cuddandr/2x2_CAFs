50f7507f07a450bc0ff3aafe5a1337da0071f024
Andrew Cudd <andrew.cudd@colorado.edu>
2023-10-19 15:41:46 -0500

    Updates to mesonless selection
    Adding track length plots and adding a cut on track length

--- mesonless.cxx
+++ mesonless.cxx
@@ -50,6 +50,8 @@ int mesonless(const std::string& file_list)
     TH1F* h_part_p_cont = new TH1F("h_part_p_cont", "h_part_p_cont", 50, 0.0, 1.0);
     TH1F* h_part_a = new TH1F("h_part_a", "h_part_a", 60, 0.0, 180);
     TH1F* h_part_a_cont = new TH1F("h_part_a_cont", "h_part_a_cont", 60, 0.0, 180);
+    TH1F* h_part_l = new TH1F("h_part_l", "h_part_l", 50, 0.0, 100);
+    TH1F* h_part_l_cont = new TH1F("h_part_l_cont", "h_part_l_cont", 50, 0.0, 100);
 
     TH1F* h_vtx_x = new TH1F("h_vtx_x", "h_vtx_x", 50, -100 + tpc_x, 100 + tpc_x);
     TH1F* h_vtx_y = new TH1F("h_vtx_y", "h_vtx_y", 50, -100 + tpc_y, 100 + tpc_y);
@@ -60,6 +62,7 @@ int mesonless(const std::string& file_list)
     TH1F* h_num_prtns = new TH1F("h_num_prtns", "h_num_prtns", 10, 0, 10);
     TH1F* h_num_phtns = new TH1F("h_num_phtns", "h_num_phtns", 10, 0, 10);
     TH1F* h_num_elecs = new TH1F("h_num_elecs", "h_num_elecs", 10, 0, 10);
+    TH1F* h_num_prims = new TH1F("h_num_prims", "h_num_prims", 10, 0, 10);
 
     auto sr = new caf::StandardRecord;
     caf_chain->SetBranchAddress("rec", &sr);
@@ -98,10 +101,12 @@ int mesonless(const std::string& file_list)
             unsigned int num_pions = 0;
             unsigned int num_phtns = 0;
             unsigned int num_elecs = 0;
+            unsigned int num_prims = 0;
             for(const auto& p : vec_part)
             {
                 if(p.primary == false)
                     continue;
+                num_prims++;
 
                 if(p.pdg == 13)
                     num_muons++;
@@ -119,6 +124,7 @@ int mesonless(const std::string& file_list)
                     num_elecs++;
             }
 
+            h_num_prims->Fill(num_prims);
             h_num_muons->Fill(num_muons);
             h_num_pions->Fill(num_pions);
             h_num_prtns->Fill(num_prtns);
@@ -132,6 +138,7 @@ int mesonless(const std::string& file_list)
                 continue;
 
             num_passed++;
+            /*
             std::cout << "[";
             for(const auto& p : vec_part)
             {
@@ -141,6 +148,7 @@ int mesonless(const std::string& file_list)
                 std::cout << p.pdg << ", ";
             }
             std::cout << "]\n";
+            */
 
             for(unsigned long ipart = 0; ipart < sr->common.ixn.dlp[ixn].part.dlp.size(); ++ipart)
             {
@@ -152,18 +160,21 @@ int mesonless(const std::string& file_list)
                 float p = std::sqrt((part.p.x * part.p.x) + (part.p.y * part.p.y) + (part.p.z * part.p.z));
 
                 auto dir = TVector3(part.end) - TVector3(part.start);
-                auto angle = dir.Angle(beam_dir) * 180.0 / TMath::Pi();
-                //auto angle = TVector3(part.p).Angle(beam_dir) * 180.0 / TMath::Pi();
+                //auto angle = dir.Angle(beam_dir) * 180.0 / TMath::Pi();
+                auto angle = TVector3(part.p).Angle(beam_dir) * 180.0 / TMath::Pi();
+                auto len = dir.Mag();
 
                 h_part_T->Fill(part.E);
                 h_part_p->Fill(p);
                 h_part_a->Fill(angle);
+                h_part_l->Fill(len);
 
-                if(part.contained)
+                if(part.contained && (len > 2.0))
                 {
                     h_part_T_cont->Fill(part.E);
                     h_part_p_cont->Fill(p);
                     h_part_a_cont->Fill(angle);
+                    h_part_l_cont->Fill(len);
                 }
             }
         }
@@ -172,6 +183,7 @@ int mesonless(const std::string& file_list)
     const std::chrono::duration<double> t_elapsed{t_end - t_start};
 
     TFile* caf_output = new TFile("plots_mesonless.root", "recreate");
+    h_num_prims->Write();
     h_num_muons->Write();
     h_num_pions->Write();
     h_num_prtns->Write();
@@ -186,6 +198,8 @@ int mesonless(const std::string& file_list)
     h_part_p_cont->Write();
     h_part_a->Write();
     h_part_a_cont->Write();
+    h_part_l->Write();
+    h_part_l_cont->Write();
     caf_output->Close();
 
     float eff = (float)num_passed / (float)total_ixn;
